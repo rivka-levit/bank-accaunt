3e95c37f8e40addacbd752eb65b5f7ba2bac965e
rivka-yuliia <juliacreadora@gmail.com>
2023-12-18 17:51:05 +0200

    more tests for timezone

--- tests/test_time_zone.py
+++ tests/test_time_zone.py
@@ -11,8 +11,8 @@ from time_zone import TimeZone
 class TestTimeZone(TestCase):
     """Tests for TimeZone class."""
 
-    def test_default_settings(self):
-        """Test the default attributes of the TimeZone class."""
+    def test_create_timezone_success(self):
+        """Test creating time zone successfully."""
 
         zone_name = 'UTC'
         tz = TimeZone(zone_name)
@@ -21,10 +21,18 @@ class TestTimeZone(TestCase):
         self.assertEqual(tz.name, zone_name)
         self.assertEqual(tz.offset, expected_offset)
 
-    def test_change_timezone(self):
-        """Test setting another timezone."""
+    def test_timezone_equal(self):
+        """Test __eq__ method of TimeZone class."""
 
-        tz = TimeZone('Asia/Jerusalem')
-        expected_offset = datetime.now(pytz.timezone(tz.name)).utcoffset()
+        tz1 = TimeZone('Asia/Jerusalem')
+        tz2 = TimeZone('Asia/Jerusalem')
 
-        self.assertEqual(tz.offset, expected_offset)
+        self.assertEqual(tz1, tz2)
+
+    def test_timezone_not_equal(self):
+        """Test not equal time zones."""
+
+        tz1 = TimeZone('Asia/Jerusalem')
+        tz2 = TimeZone('America/Los_Angeles')
+
+        self.assertNotEqual(tz1, tz2)
